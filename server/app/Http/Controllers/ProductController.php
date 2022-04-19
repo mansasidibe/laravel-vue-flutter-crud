604f181da9947a3882a3dd0b591cec53865cc724
ronphobos <sidibearouna71@gmail.com>
2022-04-19 13:22:27 +0000

    server ok

--- server/app/Http/Controllers/ProductController.php
+++ server/app/Http/Controllers/ProductController.php
@@ -19,15 +19,6 @@ class ProductController extends Controller
         return response()->json(['produits' => $product], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -67,16 +58,6 @@ class ProductController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
